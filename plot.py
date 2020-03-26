8fcfb9faef048cd9f9fffffd14e20bae42488a5a
Cody LaCoursiere <codylacoursiere@u.boisestate.edu>
2020-03-26 00:04:33 -0400

    Added stress variable to plot.py

--- plot.py
+++ plot.py
@@ -5,6 +5,7 @@ import sys
 filename = sys.argv[1]        # Stores ARG1 in filename, as in: $ python plot.py ARG1 ARG2 
 data = np.loadtxt(filename, skiprows=32, delimiter=",")   # Attempts to load filename into local variable data.
 range = (len(data))
+stress = data[:,3]
 ## Part 0
 # Figure out what arguments to add to the loadtxt function call
 # so that numbers are loaded into the local function 'data'.
